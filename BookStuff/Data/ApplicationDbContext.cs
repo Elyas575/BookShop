4253dcb94618953957c1ca15bc45cdcd0fe189ab
Elyas <Elyas575@hotmail.com>
2023-04-17 12:35:34 +0200

    First init

---
+++ BookStuff/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStuff.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace BookStuff.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
